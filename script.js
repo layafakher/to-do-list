e5ffe87927200853aaf22102b2eadb6fc87aac9f
layafakher <laya.fakher2000@gmail.com>
2021-09-03 14:55:40 +0430

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let itemList = document.querySelector('#to-do-list');
+
+let addInput = document.querySelector('#add-form input[type=text]');
+
+let searchInput = document.querySelector('#search-form input[type=text]');
